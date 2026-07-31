386c8f6f61609449a939d12c4398a4eaf9219321
Eclipsky <XuYitian726@outlook.com>
2026-07-31 23:18:55 +0800

    chore: bump version to v0.1.0-alpha.4

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 
 var CommitID string
 
-const coreVersionNumber = "v0.1.0-alpha.3"
+const coreVersionNumber = "v0.1.0-alpha.4"
 const daemonShutdownTimeout = 5 * time.Second
 
 var controlCapabilities = []string{
